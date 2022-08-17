23f46b5960ae8dfb5c70b4a155c73a66b3a0a1f2
Binay Shaw <binayshaw7777@gmail.com>
2022-08-17 22:11:43 +0530

    (+) QR Code generator

--- app/src/main/java/com/geekym/linkify/GenerateQR.kt
+++ app/src/main/java/com/geekym/linkify/GenerateQR.kt
@@ -1,11 +1,20 @@
 package com.geekym.linkify
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.geekym.linkify.databinding.FragmentGenerateQrBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+import java.util.*
 
 class GenerateQR : Fragment() {
 
@@ -18,11 +27,32 @@ class GenerateQR : Fragment() {
     ): View? {
         _binding = FragmentGenerateQrBinding.inflate(inflater, container, false)
 
+        val message = "binayshaw7777@gmail.com"
+
+        binding.qr.setImageBitmap(generateQrCode(message))
+
         return binding.root
     }
 
+    @Throws(WriterException::class)
+    fun generateQrCode(value: String): Bitmap {
+        val hintMap = Hashtable<EncodeHintType, ErrorCorrectionLevel>()
+        hintMap[EncodeHintType.ERROR_CORRECTION] = ErrorCorrectionLevel.H
+
+        val qrCodeWriter = QRCodeWriter()
+        val size = 512
+        val bitMatrix = qrCodeWriter.encode(value, BarcodeFormat.QR_CODE, size, size)
+        val width = bitMatrix.width
+        val bmp = Bitmap.createBitmap(width, width, Bitmap.Config.RGB_565)
+        for (x in 0 until width)
+            for (y in 0 until width)
+                bmp.setPixel(y, x, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+
+        return bmp
+    }
+
+
     override fun onDestroyView() {
-        // Consider not storing the binding instance in a field, if not needed.
         super.onDestroyView()
         _binding = null
     }
