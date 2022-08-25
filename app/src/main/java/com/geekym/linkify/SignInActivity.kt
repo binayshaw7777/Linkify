19882dad0101b28ab3f84fc5c641484e8a2ceb9c
Binay Shaw <binayshaw7777@gmail.com>
2022-08-25 22:44:00 +0530

    (+) Authentication Done

--- app/src/main/java/com/geekym/linkify/SignInActivity.kt
+++ app/src/main/java/com/geekym/linkify/SignInActivity.kt
@@ -3,24 +3,51 @@ package com.geekym.linkify
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.geekym.linkify.databinding.ActivitySignInBinding
 import com.geekym.linkify.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         supportActionBar?.hide()
 
         binding.createNew.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.SignInEmail.text.toString()
+            val password = binding.SignInPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                if (password.length > 8) {
+
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password length must be greater than 8", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Please enter the details!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/geekym/linkify/SignUpActivity.kt
+++ app/src/main/java/com/geekym/linkify/SignUpActivity.kt
@@ -3,18 +3,23 @@ package com.geekym.linkify
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.geekym.linkify.databinding.ActivityMainBinding
 import com.geekym.linkify.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         supportActionBar?.hide()
 
@@ -24,12 +29,28 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         binding.createAccount.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finishAffinity()
+            val name = binding.SignUpName.text.toString()
+            val email = binding.SignUpEmail.text.toString()
+            val pass = binding.SignUpPassword.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                if (pass.length > 8) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            startActivity(Intent(this, SignInActivity::class.java))
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                } else {
+                    Toast.makeText(this, "Password length must be greater than 8", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(this, "Please enter the details!", Toast.LENGTH_SHORT).show()
+            }
         }
-
-
-
     }
 }
