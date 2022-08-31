d5537a5b3a92d7791e1e44a99dc7a5ffe0afd03d
Binay Shaw <binayshaw7777@gmail.com>
2022-09-01 04:29:10 +0530

    (+) Logout

--- app/src/main/java/com/geekym/linkify/introduction/SplashScreen.kt
+++ app/src/main/java/com/geekym/linkify/introduction/SplashScreen.kt
@@ -1,28 +1,67 @@
 package com.geekym.linkify.introduction
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.geekym.linkify.MainActivity
 import com.geekym.linkify.R
+import com.geekym.linkify.authentication.SignInActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class SplashScreen : AppCompatActivity() {
 
     lateinit var handler: Handler
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
+        auth = FirebaseAuth.getInstance(); //initialize Firebase Auth
+        val currentUser: FirebaseUser? = auth.getCurrentUser() //Get the current user
 
-        handler = Handler()
-        handler.postDelayed({
+        if (currentUser == null) SendUserToLoginActivity() //If the user has not logged in, send them to On-Boarding Activity
+        else {
+            //If user was logged in last time
+            Handler().postDelayed({
+                val loginIntent: Intent
+                if (currentUser.isEmailVerified) loginIntent = Intent(this, MainActivity::class.java) //If the user email is verified
+                else loginIntent = Intent(this, SignInActivity::class.java) //If the user email is not verified
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(loginIntent)
+                finish()
+            }, 2000)
+        }
+    }
 
-            // Delay and Start Activity
-            val intent = Intent(this, Onboarding::class.java)
-            startActivity(intent)
-            finish()
-        } , 1500) // here we're delaying to startActivity after 2 seconds
+    //If the user has not logged in, send them to On-Boarding Activity
+    private fun SendUserToLoginActivity() {
+
+        // when this activity is about to be launch we need to check if its opened before or not
+        if (restorePrefData()) {
+
+            Handler().postDelayed({
+                val loginIntent = Intent(this, SignInActivity::class.java)
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(loginIntent)
+                finish()
+            }, 2000)
+        } else {
+            Handler().postDelayed({
+                val loginIntent = Intent(this, Onboarding::class.java)
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(loginIntent)
+                finish()
+            }, 2000)
+        }
+    }
+
+    private fun restorePrefData(): Boolean {
+        val pref = applicationContext.getSharedPreferences("myPrefs", MODE_PRIVATE)
+        return pref.getBoolean("isIntroOpened", false)
     }
 }
--- app/src/main/java/com/geekym/linkify/menuScreens/UserFragment.kt
+++ app/src/main/java/com/geekym/linkify/menuScreens/UserFragment.kt
@@ -1,11 +1,15 @@
 package com.geekym.linkify.menuScreens
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.geekym.linkify.authentication.SignInActivity
 import com.geekym.linkify.databinding.FragmentUserBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class UserFragment : Fragment() {
 
@@ -18,7 +22,20 @@ class UserFragment : Fragment() {
     ): View? {
 
         _binding = FragmentUserBinding.inflate(inflater, container, false)
+
+        binding.logoutButton.setOnClickListener {
+            logoutFun()
+        }
+
         return binding.root
+    }
+
+    private fun logoutFun() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(context, SignInActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        requireActivity().finish()
     }
 
     override fun onDestroyView() {
